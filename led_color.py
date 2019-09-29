f02aa6593fd840ce53fddf89a7239f66da7b05ba
Ahris <alicewang54@gmail.com>
2019-09-29 00:15:03 -0700

    Fixed a incorrect assumption

--- led_color.py
+++ led_color.py
@@ -37,14 +37,8 @@ PRIMARY_BLUE_Y = 0.08684251
 
 # Typical luminous intensity, from LED datasheet. In candela.
 RED_LUMINOUS_INTENSITY = 0.105
-RED_LUMINOUS_INTENSITY_MIN = 0.060
-RED_LUMINOUS_INTENSITY_MAX = 0.150
 GREEN_LUMINOUS_INTENSITY = 0.330
-GREEN_LUMINOUS_INTENSITY_MIN = 0.210
-GREEN_LUMINOUS_INTENSITY_MAX = 0.450
 BLUE_LUMINOUS_INTENSITY = 0.200
-BLUE_LUMINOUS_INTENSITY_MIN = 0.150
-BLUE_LUMINOUS_INTENSITY_MAX = 0.250
 
 MAX_8_BIT = 0xFF
 
@@ -206,20 +200,6 @@ def print_result(color_mixing_ratio, rgb_percentages, rgb_intensities):
     # Current` graph in the datasheet.
 
 
-def test_results(rgb_intensities):
-    if rgb_intensities[0] != 0:
-        assert rgb_intensities[0] <= RED_LUMINOUS_INTENSITY_MAX
-        assert rgb_intensities[0] >= RED_LUMINOUS_INTENSITY_MIN
-
-    if rgb_intensities[1] != 0:
-        assert rgb_intensities[1] <= GREEN_LUMINOUS_INTENSITY_MAX
-        assert rgb_intensities[1] >= GREEN_LUMINOUS_INTENSITY_MIN
-
-    if rgb_intensities[2] != 0:
-        assert rgb_intensities[2] <= BLUE_LUMINOUS_INTENSITY_MAX
-        assert rgb_intensities[2] >= BLUE_LUMINOUS_INTENSITY_MIN
-
-
 def choose_luminous_intensities(color_mixing_ratio):
     '''Find the luminous intensities that can create the target color
 
@@ -230,23 +210,16 @@ def choose_luminous_intensities(color_mixing_ratio):
     green_percentage = (color_mixing_ratio[1] / sum(color_mixing_ratio))
     blue_percentage = (color_mixing_ratio[2] / sum(color_mixing_ratio))
 
-    if round(green_percentage, 2) == 0:
-        # Use red as bounding value
-        red_intensity = RED_LUMINOUS_INTENSITY_MAX
-        green_intensity = 0
-        blue_intensity = (red_intensity / red_percentage) * blue_percentage
-    else:
-        # Use green as bounding value
-        green_intensity = GREEN_LUMINOUS_INTENSITY_MIN
-        red_intensity = (green_intensity / green_percentage) * red_percentage
-        blue_intensity = (green_intensity / green_percentage) * blue_percentage
+    # Use the red to define the bounding candela.
+    red_intensity = RED_LUMINOUS_INTENSITY
+    green_intensity = (red_intensity / red_percentage) * green_percentage
+    blue_intensity = (red_intensity / red_percentage) * blue_percentage
 
     rgb_percentages = (red_percentage, green_percentage, blue_percentage)
     rgb_intensities = (red_intensity, green_intensity, blue_intensity)
     rgb_intensities = [round(color, 2) for color in rgb_intensities]
 
     print_result(color_mixing_ratio, rgb_percentages, rgb_intensities)
-    test_results(rgb_intensities)
 
 
 def main():
